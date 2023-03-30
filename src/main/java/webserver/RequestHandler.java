85c367fe03051361ef6c77648f5fa757554bf9b0
plitche <kysu728@naver.com>
2023-03-30 23:24:25 +0900

    각 클래스 별 역할 및 설명

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Thread를 상속하고 있으며, 사용자의 요청에 대한 처리와 응답에 대한 처리를 담당하는 가증 중심 클래스
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -18,6 +19,7 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
+    // 앞으로 진행할 모든 실습은 RequestHandler 클래스의 run() 메소드에서 구현
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -6,6 +6,8 @@ import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// 웹 서버를 시작하고 사용자의 요청이 있을 때까지 대기 상태에 있다가,
+// 요청이 있을 경우 사용자의 요청을 RequestHandler 클래스에 위임하는 역할을 한다.
 public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
@@ -20,12 +22,16 @@ public class WebServer {
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
 
+        // 사용자의 요청이 발생할 때까지 대기상태에 있도록 지원하는 역할은 자바의 ServerSocket이 담당한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
+                // WebServer 클래스는 ServerSocket에 사용자의 요청이 발생하는 순간
+                // 클라이언트와 연결을 담당하는 Socket을 RequestHandler에 전달하면서
+                // 새로운 스레드를 실행하는 방식으로 멀티스레드 프로그래밍을 지원
                 RequestHandler requestHandler = new RequestHandler(connection);
                 requestHandler.start();
             }
